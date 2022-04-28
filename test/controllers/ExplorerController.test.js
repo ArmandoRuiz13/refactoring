ec136f514e1a046800e0c3050b8c4a706d24ca6e
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-28 17:07:13 -0500

    Prueba 2 obtener usernames de ExplorerController funcionando

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -7,4 +7,23 @@ describe("Prueba unitaria de ExplorerController", () =>{
         // Validamos el codigo que esperamos
         expect(explorersInNode).not.toBeUndefined();
     });
+    test("Prueba 2 usuarios que su mision es node ", () => {
+        const usernamesInNode = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(usernamesInNode).toContain("ajolonauta1");
+        expect(usernamesInNode).toContain("ajolonauta2");
+        expect(usernamesInNode).toContain("ajolonauta3");
+        expect(usernamesInNode).toContain("ajolonauta4");
+        expect(usernamesInNode).toContain("ajolonauta5");
+        expect(usernamesInNode).toContain("ajolonauta11");
+        expect(usernamesInNode).toContain("ajolonauta12");
+        expect(usernamesInNode).toContain("ajolonauta13");
+        expect(usernamesInNode).toContain("ajolonauta14");
+        expect(usernamesInNode).toContain("ajolonauta15");
+    });
+    test("Este prueba regresa los explorer de una mission", () => {
+        // Codogigo que vamos a utlizar tal cual
+        const explorersInNode = ExplorerController.getExplorersByMission("node");
+        // Validamos el codigo que esperamos
+        expect(explorersInNode).not.toBeUndefined();
+    });
 });
