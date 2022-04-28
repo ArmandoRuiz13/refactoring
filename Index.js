6977d1538f888ebb6caa23c6470f1c04d78cf26e
Armando Ruiz Anaya <armandobolt13@gmail.com>
2022-04-28 16:28:50 -0500

    Reacomodo ESLint

--- Index.js
+++ Index.js
@@ -3,11 +3,11 @@ const ExplorerService = require("./lib/services/ExplorerService");
 const FizzbuzzService = require("./lib/services/FizzbuzzService");
 
 
-const explorers = Reader.readJsonFile("explorers.json")
-console.log("Explorers clase .json \n",explorers)
+const explorers = Reader.readJsonFile("explorers.json");
+console.log("Explorers clase .json \n",explorers);
 const explorersInNode = ExplorerService.filterByMission(explorers, "node");
-console.log("Explorer con mission node", explorersInNode)
+console.log("Explorer con mission node", explorersInNode);
 const explorersAmmoutInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-console.log("cantidad Explorer con mission node", explorersAmmoutInNode)
-const usernamesInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
-console.log("Explorer usuario con mission node", usernamesInNode)
+console.log("cantidad Explorer con mission node", explorersAmmoutInNode);
+const usernamesInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+console.log("Explorer usuario con mission node", usernamesInNode);
--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -1,29 +1,29 @@
 const ExplorerService = require("./../../lib/services/ExplorerService");
 const Reader = require("./../../lib/utils/Reader");
-const explorers = Reader.readJsonFile("explorers.json")
+const explorers = Reader.readJsonFile("explorers.json");
 
-describe('Prueba unitaria para ExplorerService', () =>{
-    test('Prueba 1 Imprime la cantidad de explorers que su mission es node', () => {
+describe("Prueba unitaria para ExplorerService", () =>{
+    test("Prueba 1 Imprime la cantidad de explorers que su mission es node", () => {
         // Codogigo que vamos a utlizar tal cual
         const explorersInNode = ExplorerService.filterByMission(explorers, "node");
         // Validamos el codigo que esperamos
-        expect(explorersInNode).not.toBeUndefined()
-    })
-    test('Prueba 2 cantidad de explorers por mision', () => {
+        expect(explorersInNode).not.toBeUndefined();
+    });
+    test("Prueba 2 cantidad de explorers por mision", () => {
         const explorersAmmoutInNode = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
-        expect(explorersAmmoutInNode).toBe(10)
-    })
-    test('Prueba 3 usuarios que su mision es node ', () => {
-        const usernamesInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
-        expect(usernamesInNode).toContain('ajolonauta1')
-        expect(usernamesInNode).toContain('ajolonauta2')
-        expect(usernamesInNode).toContain('ajolonauta3')
-        expect(usernamesInNode).toContain('ajolonauta4')
-        expect(usernamesInNode).toContain('ajolonauta5')
-        expect(usernamesInNode).toContain('ajolonauta11')
-        expect(usernamesInNode).toContain('ajolonauta12')
-        expect(usernamesInNode).toContain('ajolonauta13')
-        expect(usernamesInNode).toContain('ajolonauta14')
-        expect(usernamesInNode).toContain('ajolonauta15')
-    })
-})
+        expect(explorersAmmoutInNode).toBe(10);
+    });
+    test("Prueba 3 usuarios que su mision es node ", () => {
+        const usernamesInNode = ExplorerService.getExplorersUsernamesByMission(explorers, "node");
+        expect(usernamesInNode).toContain("ajolonauta1");
+        expect(usernamesInNode).toContain("ajolonauta2");
+        expect(usernamesInNode).toContain("ajolonauta3");
+        expect(usernamesInNode).toContain("ajolonauta4");
+        expect(usernamesInNode).toContain("ajolonauta5");
+        expect(usernamesInNode).toContain("ajolonauta11");
+        expect(usernamesInNode).toContain("ajolonauta12");
+        expect(usernamesInNode).toContain("ajolonauta13");
+        expect(usernamesInNode).toContain("ajolonauta14");
+        expect(usernamesInNode).toContain("ajolonauta15");
+    });
+});
